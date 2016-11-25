298d4a23d89ef95d2521a7298698be656f174b2a
Nigel <nigelreoch@yahoo.com>
2016-11-24 21:28:32 -0500

    fetch adResponses and store

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -1,22 +1,22 @@
-import fetch from "isomorphic-fetch";
-import Promise from "bluebird";
+import request from "request";
+import vm from "vm";
 
 exports.fetchRequests = function(req, res, next) {
   let adRequests = req.body.adRequests
-  adRequests.map((request) => {
-    fetch(request, {
-      method: "get",
-      headers: new Headers({
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      })
-    }).then((response) => {
-      console.log(response.json());
+  let adResponses = [];
+  let totalRequests = adRequests.length;
+  adRequests.map((adRequest) => {
+    request(adRequest, (error, response, body) => {
+      if(response.statusCode = 200) {
+        let jsonpSandbox = vm.createContext({callbackProxy: (r) => { return r; }});
+        let requestObject = vm.runInContext(body, jsonpSandbox);
+        adResponses.push(requestObject);
+        if(adResponses.length === totalRequests) {
+          // Responses ready
+          console.log(adResponses);
+          res.send({ "hello": "world"});
+        }
+      }
     });
   });
-  Promise.all(adRequests).then((response) => {
-    //console.log(response.json());
-  });
-
-  res.send({ "hello": "world"});
 }
