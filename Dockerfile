8b621de3193a7140fb7ac5b5278c5ef42f78443a
Nigel Reoch <nigel.reoch@cbc.ca>
2017-02-20 22:45:18 -0500

    Added nginx

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,5 @@ COPY . /usr/src/app
 
 ENV NODE_ENV production
 
-EXPOSE 8000
+EXPOSE 3000
 CMD ["npm", "run", "bs"]
--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  port: process.env.PORT || 8000
+  port: process.env.PORT || 3000
 };
 
 export default config;
