afec56dc40cf5aef5e1e0dab5b37d581a4119ca7
Nigel Reoch <nigel.reoch@cbc.ca>
2017-02-23 21:40:54 -0500

    updated API url for production

--- client/actions/GptActions.js
+++ client/actions/GptActions.js
@@ -1,6 +1,6 @@
 import fetch from "isomorphic-fetch";
-const API_URL = "http://174.138.79.228/api/request";
-//const API_URL = "http://localhost/api/request";
+const API_URL = "http://138.197.35.72/api/request";
+//const API_URL = "http://localhost:8000/api/request";
 import { FETCH_AD_RESPONSES, RESET_AD_RESPONSES, SAVE_AD_REQUESTS, SAVE_SUBMITTED_REQUESTS, RESET_SAVED_RESPONSES, UPDATE_SAVED_RESPONSES } from "./types";
 
 export function resetAdResponses() {
