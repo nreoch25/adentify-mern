26adfb7f6fe90f59ce30e85768012669c429a323
Nigel Reoch <nigel.reoch@cbc.ca>
2017-03-14 17:27:46 -0400

    Added the footer panel

--- client/components/App.js
+++ client/components/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import { fetchPosts, fetchPhotos } from "../actions/AppActions";
 import gptStorage from "../utils/gptStorage";
 import Header from "./global/Header";
+import Footer from "./global/Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <div className="container top-margin">
+        <div className="container top-margin-large">
           {this.props.children}
+          <Footer />
         </div>
       </div>
     );
---
+++ client/components/global/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="panel panel-default top-margin-medium top-border-none">
+        <div className="panel-footer border-rounded">Adentify Footer</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
