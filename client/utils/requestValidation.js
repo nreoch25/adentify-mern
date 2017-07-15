9d011508d3027956a046fd34e2a83953677fdc7e
Nigel <nigelreoch@yahoo.com>
2017-07-14 21:52:47 -0400

    Network ID validation

--- client/utils/requestValidation.js
+++ client/utils/requestValidation.js
@@ -1,11 +1,18 @@
 class requestValidation {
+  static checkNetworkID(networkID) {
+    let pattern = /^\d{4,5}$/g;
+    if(pattern.test(networkID) === false) {
+      this.validationErrors.push("Invalid GPT Network ID");
+    }
+  }
   static init(gptObject) {
     console.log("GPT OBJECT VALIDATION", gptObject)
     this.validationErrors = [];
     // Test when error is returned
-    if(gptObject.networkID !== "5876") {
+    this.checkNetworkID(gptObject.networkID);
+    /*if(gptObject.networkID !== "5876") {
       this.validationErrors.push("Incorrect NetworkID used");
-    }
+    }*/
     return this.validationErrors;
   }
 }
